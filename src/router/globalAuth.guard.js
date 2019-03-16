bf5e1cd8765da8cb38030d34d2ea22df8c637f89
Wojciech Ślawski <jurigag@gmail.com>
2019-03-16 10:34:05 +0100

    [#1.4] Add global auth

---
+++ src/router/globalAuth.guard.js
@@ -0,0 +1,26 @@
+import router from './index';
+
+const AuthServiceMock = {
+  loggedIn() {
+    return localStorage.getItem('isAdmin') === 'true';
+  },
+};
+
+//11/ 11. You can create global guards
+router.beforeEach((to, from, next) => {
+  //1/ 12. Checks if user is logged in, only if requiresAuth occurs in meta
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    //1/ 13. Condition with mock service
+    if (AuthServiceMock.loggedIn()) {
+      //1/ 14. Allows to enter the route
+      next();
+    } else {
+      //4/ 15. Redirects to login page
+      next({
+        name: 'home',
+      });
+    }
+  } else {
+    next();
+  }
+});
